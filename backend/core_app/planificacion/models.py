af3694d60819a7a853720baf2f71e38ae430c76c
unknown <andresgustavorojas0@gmail.com>
2025-01-02 13:46:13 -0300

    CHANGE: Planificacion models

--- backend/core_app/planificacion/models.py
+++ backend/core_app/planificacion/models.py
@@ -1,19 +1,17 @@
 from django.db import models
 from curso.models import Curso
+from materia.mmodels import Materia
 
 class Planificacion(models.Model):
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE, related_name='planificaciones')
-    tipo = models.CharField(max_length=50)
-    contenido = models.CharField(max_length=255)
-    metodologia= models.CharField(max_length=255)
-    anotaciones = models.CharField(max_length=255)
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name='planificaciones')
+    nombre_unidad = models.CharField(max_length=100)
+    nombre_tema = models.CharField(max_length=255)
 
     def save(self, *args, **kwargs):
-        self.tipo = self.tipo.lower()
-        self.contenido = self.contenido.lower()
-        self.metodologia = self.metodologia.lower()
-        self.anotaciones = self.anotaciones.lower()
+        self.nombre_unidad = self.nombre_unidad.lower()
+        self.nombre_tema = self.nombre_tema.lower()
         super(Planificacion, self).save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.tipo}, {self.contenido}, {self.metodologia}, {self.anotaciones}"
+        return f"{self.nombre_unidad}, {self.nombre_tema}"
