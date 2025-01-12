71af31ec9a6da532c95f72e5cbd1e0928bdcc776
Cristian Delgado <cristian-31-10@hotmail.com>
2025-01-12 17:35:55 -0300

    CHANGE: limpiando archivos no usados en remoto

--- backend/core_app/seguimiento/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RegisterSeguimientoView, ListSeguimientoView
-
-urlpatterns = [
-    path('register/', RegisterSeguimientoView.as_view(), name='register_seguimiento'),
-    path('list/', ListSeguimientoView.as_view(), name='list_seguimiento'),
-]
